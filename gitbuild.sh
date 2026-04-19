009a93f6ae95dcd2fb27da63350802a0598530ef
jlamothesf <jlamothe@starfishstorage.com>
2026-04-19 17:24:16 -0500

    Initial Velantix structure

---
+++ gitbuild.sh
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "Initial Velantix structure"
+
+# Create repo on GitHub first, then:
+git branch -M main
+git remote add origin https://github.com/jlamothe0871/Velantix.git
+git push -u origin main
